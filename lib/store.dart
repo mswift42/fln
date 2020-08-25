d96fa5ee2a76559e8f5b77e8980705ceaa169872
mswift42 <martin.haesler@gmail.com>
2020-08-25 09:02:45 +0100

    fix class store.

--- lib/store.dart
+++ lib/store.dart
@@ -1,22 +1,5 @@
-class Product {
-  String title, thumbnail;
-  double price;
-  String url;
+class Store {
+  final String location, id;
 
-  Product(this.title, this.thumbnail, this.price, this.url);
-
-  factory Product.fromJson(Map<String, dynamic> json) {
-    dynamic price;
-    if (json['price'] is int) {
-      price = json['price'].toDouble();
-    } else {
-      price = json['price'];
-    }
-    return Product(
-      json['title'],
-      json['thumbnail'],
-      price,
-      json['url'],
-    );
-  }
+  const Store(this.location, this.id);
 }
