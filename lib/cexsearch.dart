2acdf3af03c6da77e19a356a38aa5f6614827e36
mswift42 <martin.haesler@gmail.com>
2020-09-01 10:50:43 +0100

    add url method.

--- lib/cexsearch.dart
+++ lib/cexsearch.dart
@@ -6,7 +6,11 @@ class CexSearch {
 
   CexSearch({this.query, this.store});
 
-  String prodUrl(String query, String storeid) {
+  String url(String query, String storeid) {
     return 'https://wss2.cex.uk.webuy.io/v3/boxes?q=$query&storeIds=[$store.location]&firstRecord=1&count=50&sortBy=sellprice&sortOrder=desc';
+  }
+
+  String prodUrl(String id) {
+    return 'https://uk.webuy.com/product-detail?id=$id';
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:fln/HomeScaffold.dart';
+import 'package:fln/widgets/HomeScaffold.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScaffold(title: 'Flutter Demo Home Page'),
+      home: HomeScaffold(title: 'cexcheck'),
     );
   }
 }
