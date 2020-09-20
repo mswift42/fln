e86cb7d03a15155ec5f8c1ad5018bf1ea7aa3070
mswift42 <martin.haesler@gmail.com>
2020-09-20 11:10:38 +0100

    add last search grid.

---
+++ lib/widgets/HomeScaffold.dart
@@ -0,0 +1,181 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:fln/cexsearch.dart';
+import 'package:fln/product.dart';
+import 'package:fln/store.dart';
+import 'package:fln/widgets/Product.dart';
+import 'package:fln/widgets/lastsearchlist.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScaffold extends StatelessWidget {
+  final String title;
+
+  HomeScaffold({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: SearchWidget(),
+      ),
+    );
+  }
+}
+
+class SearchWidget extends StatefulWidget {
+  _SearchWidgetState createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  Store activeStore = _stores[0];
+  String searchquery = "";
+  Set<String> _lastSearches = Set();
+  static final _stores = [
+    Store("Rose Street", "54"),
+    Store("Cameron Toll", "3017"),
+    Store("Leith", "3115")
+  ];
+  final controller = TextEditingController();
+
+  void _searchProduct(String inp) async {
+    if (inp != '') {
+      _lastSearches.add(inp);
+      var cs = CexSearch(query: searchquery, store: activeStore);
+      Navigator.of(context).push(
+        new MaterialPageRoute(
+          builder: (context) => Scaffold(
+            appBar: AppBar(
+                title: Text(
+              "Showing $searchquery at ${activeStore.location}.",
+              textScaleFactor: 0.7,
+            )),
+            body: _showResultsBody(cs.fetchProduct()),
+          ),
+        ),
+      );
+    }
+  }
+
+  void _setSearchQueryText() {
+    searchquery = controller.text;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(_setSearchQueryText);
+  }
+
+  void handleActiveStoreChanged(Store store) async {
+    setState(() {
+      activeStore = store;
+    });
+    _searchProduct(searchquery);
+  }
+
+  void _handlePillTap(String inp) {
+    setState(() {
+      searchquery = inp;
+      controller.text = inp;
+      _searchProduct(searchquery);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(_setSearchQueryText);
+    controller.dispose();
+    super.dispose();
+  }
+
+  String prodUrl(String query, String storeid) {
+    return 'https://cxchecker.appspot.com/querycx?query=$query&location=$storeid';
+  }
+
+  Future<List<Product>> fetchProduct(String query, String storeid) async {
+    final url = prodUrl(query, storeid);
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var decoded = json.decode(response.body) as List;
+      if (decoded != null) {
+        return decoded.map((i) => Product.fromJson(i)).toList();
+      } else {
+        return [];
+      }
+    } else {
+      throw Exception("Failed to load product");
+    }
+  }
+
+  void _handleDelete(String value) {
+    setState(() {
+      _lastSearches.remove(value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: TextField(
+              controller: controller,
+              onSubmitted: _searchProduct,
+            ),
+            padding: EdgeInsets.symmetric(vertical: 6.00, horizontal: 8.00),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: _stores
+                .map((i) =>
+                    _radioWidget(i, activeStore, handleActiveStoreChanged))
+                .toList(),
+          ),
+          LastSearchGrid(_handleDelete, _handlePillTap, _lastSearches.toList())
+        ],
+      ),
+    );
+  }
+}
+
+Widget _radioWidget(
+    Store value, Store groupvalue, ValueChanged<Store> handler) {
+  return Row(
+    children: <Widget>[
+      Text(value.location),
+      Radio<Store>(
+        value: value,
+        groupValue: groupvalue,
+        onChanged: handler,
+      ),
+    ],
+  );
+}
+
+FutureBuilder<List<Product>> _showResultsBody(Future<List<Product>> handler) {
+  return FutureBuilder(
+    future: handler,
+    builder: (BuildContext context, AsyncSnapshot<List<Product>> snapshot) {
+      switch (snapshot.connectionState) {
+        case ConnectionState.none:
+          return Container(child: Center(child: Text("Please try again.")));
+        case ConnectionState.active:
+        case ConnectionState.waiting:
+          return Container(child: Center(child: CircularProgressIndicator()));
+        case ConnectionState.done:
+          if (snapshot.hasError) {
+            return Text("Something went wrong: ${snapshot.error}");
+          }
+          return ProductsWidget(snapshot.data);
+      }
+      return null;
+    },
+  );
+}
